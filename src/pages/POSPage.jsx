be93d4c6770f573f005138c8781bca2961c24095
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-05-07 16:24:25 +0500

    feat:for getting data from backend installing axios

--- src/pages/POSPage.jsx
+++ src/pages/POSPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import MainLayout from "../layout/MainLayout";
 
 export default function POSPage() {
-  return <div>POSPage</div>;
+  return <MainLayout>POS System</MainLayout>;
 }
