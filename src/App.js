fe07aba0444f0cfe7d72dee83516c3113ff7dbca
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-05-07 16:58:51 +0500

    feat:Fetched data from backend using axios

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import POSPage from "./pages/POSPage";
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle";
 function App() {
   return (
     <BrowserRouter>
--- src/pages/POSPage.jsx
+++ src/pages/POSPage.jsx
@@ -1,6 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainLayout from "../layout/MainLayout";
+import axios from "axios";
 
 export default function POSPage() {
-  return <MainLayout>POS System</MainLayout>;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchProducts = async () => {
+    setIsLoading(true);
+    const res = await axios.get("http://localhost:5000/products");
+    setProducts(await res.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <MainLayout>
+      <div className="row">
+        <div className="col-lg-8">
+          {isLoading ? (
+            "Loading"
+          ) : (
+            <div className="row">
+              {products.map((product, key) => (
+                <div key={key} className="col-lg-4">
+                  <div className="border">
+                    <p>{product.name}</p>
+                    <img
+                      src={product.image}
+                      className="img-fluid"
+                      alt={product.name}
+                    />
+                    <p>{product.price}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="col-lg-4"></div>
+      </div>
+    </MainLayout>
+  );
 }
